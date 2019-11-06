eefd095f1e05fa21eccd174a1c73cb60bfad71c7
Sabrya van Heijst <s.e.vanheijst@tudelft.nl>
2019-11-06 13:31:27 +0100

    Added circumference function

--- test.py
+++ test.py
@@ -1,1 +1,16 @@
 print('hello world')
+print('-----------')
+
+import numpy as np
+
+# circumference circle
+def circ(r):
+	"""This function will calculate the circumference of a circle of radius r.
+
+	Parameters: radius, r
+	Returns: circumference"""
+	return 2*np.pi*r
+
+R=float(input('Give the radius of the circle [cm]: '))
+C=circ(R)
+print('A circle with a radius of %0.2f cm has a circumference of %0.2f cm' %(R,C))
