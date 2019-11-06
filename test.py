d8d1a84c5ae6c64dd23852626bbd3494831139f5
Sabrya van Heijst <s.e.vanheijst@tudelft.nl>
2019-11-06 15:39:49 +0100

    Cleaned up file

--- test.py
+++ test.py
@@ -1,21 +1,21 @@
+# Import all thing required
+import numpy as np
+
+# Prints only needed in first exercise
 if __name__ == '__main__':
     print('hello world')
     print('-----------')
 
-import numpy as np
 
+## Functions
 # circumference circle
 def circ(r):
-	"""This function will calculate the circumference of a circle of radius r.
-
-	Parameters: radius, r
-	Returns: circumference"""
-	return 2*np.pi*r
-
-## used to check if the script was working
-#R=float(input('Give the radius of the circle [cm]: '))
-#C=circ(R)
-#print('A circle with a radius of %0.2f cm has a circumference of %0.2f cm' %(R,C))
+    """This function will calculate the circumference of a circle of radius r.
+    
+    Parameters: radius, r
+    
+    Returns: circumference"""
+    return 2*np.pi*r
 
 # surface circle
 def surface(r):
@@ -25,12 +25,18 @@ def surface(r):
 	Returns: surface"""
 	return np.pi*r**2
 
-## used to check if the script was working
-#S=surface(R)
-#print('A circle with a radius of %0.2f cm has a surface of %0.2f cm^2' %(R,S))
-
-# define circle
+# convert polar cordinates to cartesian
 def polar2cart(r,theta=np.linspace(0,2*np.pi,100)):
     x=r*np.cos(theta)
     y=r*np.sin(theta)
     return x, y
+
+
+## Prints used to check if my script was working, before creation script.py
+if __name__ == '__main__':
+    R=float(input('Give the radius of the circle [cm]: '))
+    C=circ(R)
+    print('A circle with a radius of %0.2f cm has a circumference of %0.2f cm' %(R,C))
+    
+    S=surface(R)
+    print('A circle with a radius of %0.2f cm has a surface of %0.2f cm^2' %(R,S))
