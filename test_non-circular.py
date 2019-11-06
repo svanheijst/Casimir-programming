c7a3fa278898a0e8fd93ba76329da7fe363ee9c8
Sabrya van Heijst <s.e.vanheijst@tudelft.nl>
2019-11-06 15:47:09 +0100

    New functions for different shapes

---
+++ test_non-circular.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+def triangle_area(b,h):
+    """This function calculated the area of a traingle.
+    
+    Parameters: base and height of the triangle
+    Returns: area"""
+    return b*h/2
+
+
+def square(s):
+    """This function calculated the area of a square.
+    
+    Parameters: side length of square.
+    Returns: area"""
+    return s*s
