1a1e58f1ee07f1e98b3c86f726759af5aeca89c6
Maarten Bolhuis <m.bolhuis@tudelft.nl>
2019-11-06 14:48:21 +0100

    Plot x,y

--- script.py
+++ script.py
@@ -1,3 +1,4 @@
+import numpy as np
 print('made by Maarten Bolhuis')
 
 print('This script calls on the script test')
@@ -11,3 +12,11 @@ C = test.circ(Radius)
 S = test.surface(Radius)
 
 print('A circle of radius %0.2f cm has a circumference of %0.2f cm and a surface of %0.2f cm^2' %(Radius,C,S))
+
+import matplotlib.pyplot as plt
+x = np.linspace(0,11,10)
+y = 2*x
+fig = plt.figure()
+ax1 = fig.add_subplot(111)
+ax1.plot(x,y)
+plt.show()
