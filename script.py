ee91008b54f5a6ad45cb9315fe5e7e04ce7886b5
Maarten Bolhuis <m.bolhuis@tudelft.nl>
2019-11-06 15:27:50 +0100

    Plot is now seize 4x4

--- script.py
+++ script.py
@@ -1,4 +1,4 @@
-#%matplotlib inline
+# %matplotlib inline
 import numpy as np
 print('made by Maarten Bolhuis')
 
@@ -17,7 +17,10 @@ print('A circle of radius %0.2f cm has a circumference of %0.2f cm and a surface
 # plot circle
 import matplotlib.pyplot as plt
 x,y=test.polar2cart(Radius)
-fig = plt.figure()
+fig = plt.figure(figsize=(4,4))
 ax1 = fig.add_subplot(111)
 ax1.plot(x,y)
+plt.show()
 fig.savefig('Plot')
+
+
